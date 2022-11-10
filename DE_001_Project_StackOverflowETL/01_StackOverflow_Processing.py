53c92486c0ba08bf418b8a7af3436b2a5a6ab7f9
lmirandad27@gmail.com <lmirandad27@gmail.com>
2022-11-10 18:09:03 +0000

    Commit 20221110_1

--- DE_001_Project_StackOverflowETL/01_StackOverflow_Processing.py
+++ DE_001_Project_StackOverflowETL/01_StackOverflow_Processing.py
@@ -26,7 +26,7 @@ df_post_questions.printSchema()
 column_post_questions = ['id','title','accepted_answer_id','answer_count','comment_count',
                           'creation_date','owner_user_id','score','tags','view_count']
 
-df_post_questions = df_post_questions.select(columns_post_questions).filter(year('creation_date')>=2020)
+df_post_questions = df_post_questions.select(column_post_questions).filter(year('creation_date')>=2020)
 
 # COMMAND ----------
 
@@ -104,7 +104,74 @@ display(df_badges.select(column_badges).filter("""tag_based = false""").groupBy(
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC #### 1- Most frequent questions per technology (tags) - Top 15
+# MAGIC #### 1- Table Post Question Level
+
+# COMMAND ----------
+
+#Joining Questions and Answers
+column_post_a = [col('id').alias('id_answer'),col('creation_date').alias('answer_date'),col('owner_user_id').alias('user_id_answer'),
+                 col('score').alias('answer_score')]
+
+df_post_answers = df_post_answers.select(column_post_a)
+
+df_post_questions_answers = df_post_questions\
+                                .join(df_post_answers,df_post_questions.accepted_answer_id == df_post_answers.id_answer,how='left')
+
+# COMMAND ----------
+
+display(df_post_questions_answers.select(col('id'),col('title'),col('tags'),col('creation_date'),col('answer_date'),
+                                datediff(col('answer_date'),col('creation_date'))))
+
+# COMMAND ----------
+
+#Let's create some custome aggregations
+
+#For tag counting
+sum_total_1_tag = sum(when(size(split(col('tags'),"\|")) == 1,lit(1)).otherwise(lit(0)))
+sum_total_2_tag = sum(when(size(split(col('tags'),"\|")) == 2,lit(1)).otherwise(lit(0)))
+sum_total_3_tag = sum(when(size(split(col('tags'),"\|")) == 3,lit(1)).otherwise(lit(0)))
+sum_total_4_tag = sum(when(size(split(col('tags'),"\|")) == 4,lit(1)).otherwise(lit(0)))
+sum_total_5m_tag = sum(when(size(split(col('tags'),"\|")) >= 5,lit(1)).otherwise(lit(0)))
+
+#For questions with accepted answers
+sum_quest_accepted_answer = sum(when(~col('accepted_answer_id').isNull(),lit(1)).otherwise(lit(0)))
+
+#For delay of accepted answer
+avg_days_accepted_answer = avg(datediff(col('answer_date'),col('creation_date')))
+max_days_accepted_answer = max(datediff(col('answer_date'),col('creation_date')))
+min_days_accepted_answer = min(datediff(col('answer_date'),col('creation_date')))
+
+
+# COMMAND ----------
+
+df_post_questions_grouped = df_post_questions_answers\
+            .select('*',
+                    explode(split(col('tags'),"\|")).alias('tag_name'),
+                    year(col('creation_date')).alias('year'),
+                    month(col('creation_date')).alias('month')
+                   )\
+            .groupBy('year','month')\
+            .agg(count('*').alias('qty_questions'),
+                 sum_quest_accepted_answer.alias('sum_questions_w_acceptedanswer'),
+                 avg_days_accepted_answer.alias('avg_days_accepted_answer'),
+                 max_days_accepted_answer.alias('max_days_accepted_answer'),
+                 min_days_accepted_answer.alias('min_days_accepted_answer'),
+                 sum('answer_count').alias('sum_answers'),
+                 avg('answer_count').alias('avg_answers'),
+                 sum('comment_count').alias('sum_comments'),
+                 avg('comment_count').alias('avg_comments'),
+                 sum('view_count').alias('sum_views'),
+                 avg('view_count').alias('avg_views'),
+                 sum_total_1_tag.alias('sum_total_1_tag'),
+                 sum_total_2_tag.alias('sum_total_2_tag'),
+                 sum_total_3_tag.alias('sum_total_3_tag'),
+                 sum_total_4_tag.alias('sum_total_4_tag'),
+                 sum_total_5m_tag.alias('sum_total_5m_tag')
+                )\
+            .orderBy(col('year').desc(),col('month').desc())
+
+display(df_post_questions_grouped)
+
 
 # COMMAND ----------
 
